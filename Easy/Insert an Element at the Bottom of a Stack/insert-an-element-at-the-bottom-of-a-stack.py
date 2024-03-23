ecc46dcf6c485451ab2cd11b358a185b4832357c
Adil Ahmad <91419013+ADIL0025@users.noreply.github.com>
2024-03-24 00:05:15 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Insert an Element at the Bottom of a Stack/insert-an-element-at-the-bottom-of-a-stack.py
@@ -0,0 +1,27 @@
+#User function Template for python3
+class Solution:
+    def insertAtBottom(self,st,x):
+        # code here
+        st = st[::-1] 
+        st.append(x) 
+        st = st[::-1] 
+        return st
+
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        n,x = map(int,input().split())
+        stack = list(map(int,input().split()))
+        obj = Solution()
+        ans = obj.insertAtBottom(stack,x)
+        for e in ans:
+            print(e,end=" ")
+        print()
+        
+        
+        
+# } Driver Code Ends
