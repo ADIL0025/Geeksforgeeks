f9eab668cd7cc842de95abdaea4b675cefbcfef4
Adil Ahmad <91419013+ADIL0025@users.noreply.github.com>
2025-01-09 00:57:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    vector<int> subarraySum(vector<int> &arr, int target) {
+        // code here
+        int i = 0;
+        int n = arr.size();
+        int j = 0;
+        int sum = 0;
+        vector<int> ans(2);
+        while(j < n){
+            sum += arr[j];
+            while(sum > target && i < j){
+                sum -= arr[i];
+                i++;
+            }
+            if(sum == target){
+                ans[0] = i+1;
+                ans[1] = j+1;
+                return ans;
+            }
+            j++;
+        }
+        return {-1};
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        int d;
+        string input;
+
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        cin >> d;
+        cin.ignore();
+
+        Solution ob;
+        vector<int> result = ob.subarraySum(arr, d);
+        for (int i : result) {
+            cout << i << " ";
+        }
+        cout << "\n~\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
